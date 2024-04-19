82de2cc283e3ec58987c0024172b2f4176dd8781
IshikawaTakuma0716 <164276908+IshikawaTakuma0716@users.noreply.github.com>
2024-04-19 14:28:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
 
+function getFortune() {
+    var fortunes = ['大吉', '中吉', '小吉', '吉', '凶'];
+    var randomIndex = Math.floor(Math.random() * fortunes.length);
+    var fortune = fortunes[randomIndex];
+    
+    document.getElementById('fortune').innerHTML = '今日の運勢: ' + fortune;
+  }
